daaffad9b47471702ee384feb409df423a4aec19
tomekk <jansen@hnm.ch>
2019-01-07 09:58:23 +0100

    Update Attachment

---
+++ src/app/ci/common/ciAttachment.html.php
@@ -0,0 +1,27 @@
+<?php 
+	use n2n\impl\web\ui\view\html\HtmlView;
+	use ci\common\CiAttachment;
+	use n2n\impl\web\ui\view\html\HtmlUtils;
+
+	$view = HtmlView::view($view);
+	$html = HtmlView::html($view);
+
+	$ciAttachment = $view->getParam('ciAttachment');
+	$view->assert($ciAttachment instanceof CiAttachment);
+	
+	$aAttr = array('target' => 'blank', 'class' => 'ci-attachment file', 'download');
+	$file = $ciAttachment->getFile();
+	
+	$aAttr = HtmlUtils::mergeAttrs($aAttr, array('class' => $ciAttachment->isNested() ? 'ci-item-nested' : 'ci-item'));
+
+	if (null !== $file && $file->isValid()) {
+		$aAttr = HtmlUtils::mergeAttrs($aAttr, array('class' => $file->getOriginalExtension()));
+	}
+?>
+
+<?php $html->linkStart($file, $aAttr, 'div') ?>
+	<h3 class="ci-attachment__title h2"><?php $html->out($ciAttachment->getLabel()) ?></h3>
+	<?php if (null !== ($desc = $ciAttachment->getDescription())): ?>
+		<p class="ci-attachment__description"><?php $html->out($desc)?></p>
+	<?php endif ?>
+<?php $html->linkEnd() ?>
