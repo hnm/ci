b2c514239e7d902d5bf7377ee0c1979c33c8f24a
tomekk <jansen@hnm.ch>
2019-03-19 16:27:54 +0100

    update youtube: specs, migrate, install

--- src/var/etc/ci/install.my.sql
+++ src/var/etc/ci/install.my.sql
@@ -102,7 +102,7 @@ CREATE TABLE IF NOT EXISTS `ci_two_columns_content_items` (
 
 CREATE TABLE IF NOT EXISTS `ci_youtube` (
   `id` int(10) unsigned NOT NULL,
-  `youtube_id` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `youtube_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `nested_ci_type` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
